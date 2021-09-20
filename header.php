e2f3878acb3a8bd8f51308556792916195ef8941
Johanna Jimeno <johannajimenoo@gmail.com>
2021-09-21 00:28:17 +0800

    Separated the nav and header files
    For reusability

---
+++ header.php
@@ -0,0 +1,14 @@
+<!--jQuery-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!--Bootstrap-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<!--OneMap API-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.css" />
+<script src="https://cdn.onemap.sg/leaflet/onemap-leaflet.js"></script>
+
+<!--Custom-->
+<link rel="stylesheet" href="style.css">
+<script src="script.js"></script>
