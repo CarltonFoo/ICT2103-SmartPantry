0992baf0bfb9331b4bcf5643d0a3c36e8d8599fa
Z-Jian <2002689@sit.singaporetech.edu.sg>
2021-10-30 14:02:48 +0800

    Renamed file from csv.py and filecheck feature
    Avoid conflict with csv.py in pandas and added feature to check if file exists in datasets folder

--- backend/Objects/importCSV.py
+++ backend/Objects/importCSV.py
@@ -5,10 +5,12 @@ class csv:
     """A CSV object represent the CSV."""
     def __init__(self, file: str) :
         if not file.endswith('.csv'):
-            raise Exception("<File format> is not supported. Only supports CSV file format and ensure file is stored in datasets folder")
-        script_dir = os.path.dirname(__file__)
-        self.dir = os.path.join(script_dir, "../datasets/" + file)
-        self.dirname, self.filname= os.path.split(os.path.abspath(self.dir))
+            raise Exception("File format is not supported. Only supports CSV.")
+        dir = os.path.join(os.getcwd(), "backend/datasets/", file)
+        if not os.path.exists(dir):
+            raise Exception("File does not exist in datasets folder")
+        self.dir = dir
+        self.dirname, self.filname= os.path.split(self.dir)
         self.data = self.import_csv()
 
     def  import_csv(self):
