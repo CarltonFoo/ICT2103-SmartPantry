15d428e16c512308280fbe7b0cc9bf953922a155
Z-Jian <2002689@sit.singaporetech.edu.sg>
2021-11-05 11:04:36 +0800

    Update importCSV.py
    Added a function that inserts dataframe into sql database

--- backend/Objects/importCSV.py
+++ backend/Objects/importCSV.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import sqlalchemy
 
 class csv:
     """A CSV object represent the CSV."""
@@ -16,3 +17,7 @@ class csv:
     def  import_csv(self):
         """This imports CSV and returns a dataframe, expecting columns header"""
         return pd.read_csv(self.dir)
+
+    def  csv_to_sql(self, engine: sqlalchemy.engine.Engine, table: str, ifExists = 'append', chunkSize = 1000):
+        """This function inserts Pandas DataFrames into a Database"""
+        self.data.to_sql(table, con=engine,if_exists=ifExists, chunksize= chunkSize)
