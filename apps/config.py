f901443bad6535266fbf9fd6e02f77a1f62a3af6
Z-Jian <2002689@sit.singaporetech.edu.sg>
2021-11-18 22:37:21 +0800

    modified config

--- apps/config.py
+++ apps/config.py
@@ -10,11 +10,15 @@ class Config(object):
 
     basedir = os.path.abspath(os.path.dirname(__file__))
 
+    # Set up SQL username and password
+    SQL_USERNAME=os.getenv("SQL_USERNAME")
+    SQL_PASSWORD = os.getenv("SQL_PASSWORD")
+
     # Set up the App SECRET_KEY
     SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
 
     # This will create a file in <app> FOLDER
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost:3306/smartpantry'
+    SQLALCHEMY_DATABASE_URI = f'mysql://{SQL_USERNAME}:{SQL_PASSWORD}@localhost:3306/smartpantry'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DebugConfig(Config):
