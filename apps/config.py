99dd67e3ef1e854640bfbdd0ea166b469c965f2e
Z-Jian <2002689@sit.singaporetech.edu.sg>
2021-11-19 18:07:00 +0800

    Modified config and updated requirements.txt

--- apps/config.py
+++ apps/config.py
@@ -7,18 +7,19 @@ import os
 from decouple import config
 
 class Config(object):
+    SQL_USERNAME=os.getenv("SQL_USERNAME")
+    SQL_PASSWORD = os.getenv("SQL_PASSWORD")
+    SQL_DATABASE=os.getenv("SQL_DATABASE")
 
     basedir = os.path.abspath(os.path.dirname(__file__))
 
     # Set up SQL username and password
-    SQL_USERNAME=os.getenv("SQL_USERNAME")
-    SQL_PASSWORD = os.getenv("SQL_PASSWORD")
 
     # Set up the App SECRET_KEY
     SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
 
     # This will create a file in <app> FOLDER
-    SQLALCHEMY_DATABASE_URI = f'mysql://{SQL_USERNAME}:{SQL_PASSWORD}@localhost:3306/smartpantry'
+    SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{SQL_USERNAME}:{SQL_PASSWORD}@localhost:3306/{SQL_DATABASE}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DebugConfig(Config):
