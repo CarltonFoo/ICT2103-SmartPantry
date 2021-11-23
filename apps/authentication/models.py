f9fb331a7a87486ababc2bddcc3a639a89242eb4
Johanna Jimeno <johannajimenoo@gmail.com>
2021-11-23 20:58:50 +0800

    Revert "Merge branch 'main' into johanna"
    This reverts commit 76af446cb8b0c6751f6c9962c3cfa929f2733b60, reversing
    changes made to bb9c354f3d9231402bc110b428b462cce88ffee9.

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -4,40 +4,11 @@ from apps import db, login_manager
 
 from apps.authentication.util import hash_pass
 
-class FoodItems(db.Model):
-    __tablename__ = 'food_item'
-    fid = db.Column(db.Integer, primary_key=True)
-    food_name = db.Column(db.String(255))
-    price = db.Column(db.Float)
-    weight = db.Column(db.Float)
-    calories= db.Column(db.Integer)
-class Receipts(db.Model):
-    __tablename__ = 'receipt'
-    rid =db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    type = db.Column(db.String(255))
-    calories=  db.Column(db.Integer)
-    description= db.Column(db.String(255))
-    dietary_needs= db.Column(db.String(255))
-class Receipts_Ingredient(db.Model):
-    __tablename__ = 'receipt_ingredient'
-    rid =db.Column(db.Integer, primary_key=True)
-    fid = db.Column(db.String(255))
-    weight = db.Column(db.Float)
-
-class pantry(db.Model):
-    __tablename__ = 'pantry'
-
-    uid = db.Column(db.Integer, primary_key=True)
-    fid = db.Column(db.Integer, primary_key=True)
-    weight = db.Column(db.FLOAT())
-
-
 class Users(db.Model, UserMixin):
 
     __tablename__ = 'user'
 
-    uid = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     gender = db.Column(db.String(1))
@@ -76,6 +47,3 @@ def request_loader(request):
     username = request.form.get('username')
     user = Users.query.filter_by(username=username).first()
     return user if user else None
-
-
-
