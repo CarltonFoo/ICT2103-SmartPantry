38a3c550de59bc99ce7a2111f90993a3c3460414
Z-Jian <2002689@sit.singaporetech.edu.sg>
2021-11-23 13:55:07 +0800

    Model meal history

---
+++ Model/meal_history.py
@@ -0,0 +1,42 @@
+from Model.base import Item
+from base import Base, Item
+
+class MealHistory(Base):
+    def __init__(self, data) -> None:
+        Base.__init__(data)
+        if self.hasData(data, "date", "meal_type", "rid", "uid"):
+            self.__date = data["date"]
+            self.__meal_type = data["meal_type"]
+            self.__rid = data["rid"]
+            self.__uid = data["uid"]
+
+    def setDate(self, data):
+        if self.hasData(data, "date"):
+            self.__date = data["date"]
+
+    def setMealType(self, data):
+        if self.hasData(data, "meal_type"):
+            self.__meal_type = data["meal_type"]
+
+    def setRid(self, data):
+        if self.hasData(data, "rid"):
+            self.__rid = data["rid"]
+
+    def setDate(self, data):
+        if self.hasData(data, "uid"):
+            self.__uid = data["uid"]
+
+    def getDate(self, data):
+        return {"date": self.__date}
+
+    def getMealType(self, data):
+        return {"meal_type": self.__meal_type}
+
+    def getRid(self, data):
+        return {"rid": self.__rid}
+
+    def getUid(self, data):
+        return {"uid": self.__uid}
+
+    def getter(self):
+        return {**self.getID(), **self.getDate, **self.getMealType(), **self.getRid(), **self.getUid()}
