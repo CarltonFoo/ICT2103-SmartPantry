a41c86d864b61e6a32f69d5dfba0cabcaa88b0f0
CarltonFoo <carltonfoo@gmail.com>
2021-12-05 17:32:24 +0800

    update sidebar and mealhistory sql

--- Objects/sql_scripts/mealhistory.sql
+++ Objects/sql_scripts/mealhistory.sql
@@ -1,11 +1,18 @@
-CREATE TABLE IF NOT EXISTS MealHistory ( 
- rid INT NOT NULL,
- uid INT NOT NULL,
- date DATETIME NOT NULL,
- type VARCHAR(255) NOT NULL,
- FOREIGN KEY(rid) REFERENCES recipe(rid),
- FOREIGN KEY(uid) REFERENCES user(uid),
- FOREIGN KEY(type) REFERENCES recipe(type),
- PRIMARY KEY(rid,uid)
- );
- 
+CREATE TABLE IF NOT EXISTS mealhistory (
+  rid INT NOT NULL,
+  id INT NOT NULL,
+  date DATETIME NOT NULL,
+  FOREIGN KEY(rid) REFERENCES recipe(rid),
+  FOREIGN KEY(id) REFERENCES user(id),
+  PRIMARY KEY(rid, id)
+);
+
+
+INSERT INTO `receipt_ingredient` VALUES 
+(3,1,'2021-12-01'),
+(1,1,'2021-11-02'),
+(2,2,'2021-11-03'),
+(5,2,'2021-11-03'),
+(7,3,'2021-11-03'),
+(6,3,'2021-11-03'),
+(4,4,'2021-11-04');
